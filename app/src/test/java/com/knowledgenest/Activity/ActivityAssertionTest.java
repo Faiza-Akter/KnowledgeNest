eab09cac495dd7a562c3608e1216a990937b8e1a
tasnia2 <tasnia340@gmail.com>
2025-08-03 20:28:45 +0600

    assertion in ActivityAssertionTest.java

--- app/src/test/java/com/knowledgenest/Activity/ActivityAssertionTest.java
+++ app/src/test/java/com/knowledgenest/Activity/ActivityAssertionTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ActivityAssertionTest {
-    //MainActivity Logic Tests
+    // MainActivity Logic Tests
     @Test
     void testToolbarTitleSwitch() {
         int HOME = 101;
